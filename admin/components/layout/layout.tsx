81a9d06807bbf48fbc5953766641b29a4c8bc6b5
otterp012 <otterp012@gmail.com>
2022-08-24 02:50:34 +0900

    add px

--- admin/components/layout/layout.tsx
+++ admin/components/layout/layout.tsx
@@ -10,7 +10,7 @@ type LayoutProps = {
 const Layout: React.FC<LayoutProps> = ({ children, data }) => {
   return (
     <div className='min-h-screen w-[1140px] px-5 space-y-5 '>
-      <header className='w-full h-28 flex items-center justify-between bg-main px-2 py-2'>
+      <header className='w-full h-28 flex items-center justify-between bg-main px-6 py-2'>
         <div className='flex items-center'>
           <BigLogo color='white' />
           <h1 className='text-4xl ml-10 font-bold text-white'>리뷰 관리</h1>
